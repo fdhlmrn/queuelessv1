c8c7933f6ed8987e2af42e352e2c1d47f3117483
fdhlmrn <thepadei@gmail.com>
2019-07-29 22:45:19 +0800

    add product api controller for segregation

---
+++ app/Http/Controllers/ProductAPIController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('product.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        return Product::create($request->all());
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function show(product $product)
+    {
+
+        return Product::find($product->id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(product $product)
+    {
+
+        $product = Product::findOrFail($product->id);
+        return view('product.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, product $product)
+    {
+
+        $product = Product::findOrFail($product->id);
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\products  $products
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(product $product)
+    {
+
+        $product = Product::findOrFail($product->id);
+        $product->delete();
+
+        return 204;
+    }
+}
